c1f2920c9725ad9d64425ad279952edb93665f07
tktk <tktk.stereoman@gmail.com>
2015-11-10 21:17:35 +0900

    implementing APIs...

--- opc_test.rb
+++ opc_test.rb
@@ -7,11 +7,11 @@ HOST = "http://192.168.0.10"
 USER_AGENT = "OlympusCameraKit"
 
 # カメラを撮影モードに変更
-cameramode_rec = "/switch_cameramode.cgi?mode=rec"
+cameramode_rec = "/switch_cameramode.cgi?mode=rec"#&lvqty=0640x0480"
 # カメラをスタンドアロンモードに変更
 cameramode_standalone = "/switch_cameramode.cgi?mode=standalone"
 # 撮影開始
-exec_takemotion = "/exec_takemotion.cgi?com=newstarttake&point=0600x0200"
+exec_takemotion = "/exec_takemotion.cgi?com=newstarttake"#&point=0600x0200"
 # 電源OFF
 exec_pwoff = "/exec_pwoff.cgi"
 # コマンド受付方法の取得
@@ -28,6 +28,9 @@ get_connectmode = "/get_connectmode.cgi"
 exec_misc_zoom_start_tele = "/exec_takemisc.cgi?com=newctrlzoom&ctrl=start&dir=tele&method=contslow"
 exec_misc_zoom_start_wide = "/exec_takemisc.cgi?com=newctrlzoom&ctrl=start&dir=wide&method=contslow"
 exec_misc_zoom_stop = "/exec_takemisc.cgi?com=newctrlzoom&ctrl=stop"
+# ライブビュー
+exec_misc_start_liveivew = "/exec_takemisc.cgi?com=startliveview&port=5555"
+exec_misc_stop_liveivew = "/exec_takemisc.cgi?com=stopliveview"
 
 def exec_command(command)
 	header = {
@@ -57,18 +60,23 @@ socket = TCPSocket.open("192.168.0.10", 65000)
 # 動作モード切り替え スタンドアロン
 puts exec_command(cameramode_standalone).body
 
-#sleep 1
-
 # 動作モード切り替え 撮影モード
 res = exec_command(cameramode_rec)
 puts res.body
 
+# ライブビュー開始
+puts exec_command(exec_misc_start_liveivew).body
+
+=begin
 # ワイドにしてズームする
 puts exec_command(exec_misc_zoom_start_wide).body
-sleep 1
+sleep 2
 puts exec_command(exec_misc_zoom_start_tele).body
-sleep 1
+sleep 2
 puts exec_command(exec_misc_zoom_stop).body
+=end
+
+sleep 1
 
 # 撮影
 r = exec_command(exec_takemotion)
@@ -77,5 +85,8 @@ puts r.body
 
 # カメラ状態取得
 puts exec_command(get_state).body
+
+# ライブビュー停止
+puts exec_command(exec_misc_stop_liveivew)
 
 socket.close
