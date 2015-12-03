2113ab5d9501a49c9a1e5b263539532a02d2edcb
tktk <tktk.stereoman@gmail.com>
2015-12-03 17:44:03 +0900

    refactoring

--- opc_rtp_parser.rb
+++ opc_rtp_parser.rb
@@ -12,7 +12,6 @@ class RTPParser
 
 	def connect
 		# websocketコネクション
-		#@ws = WebSocket::Client::Simple.connect 'ws://localhost:8080'
 		EM.run {
 			@ws = Faye::WebSocket::Client.new('ws://localhost:8080', [], {
 				:headers => {'User-Agent' => 'ruby'}
@@ -23,36 +22,31 @@ class RTPParser
 				@ws.send('hello')
 			end
 
-			#@ws.on :message do |event|
-			#	p [:message, event.data]
-			#end
-
 			@ws.on :close do |event|
 				p [:close, event.code, event.reason]
 			end	
 		}
 	end
 
+	# RTPパケットをパースする 
 	def parse pkt
-		# RTPパケットをパースする 
 		# RTPヘッダ
+		# pkt[x]あたり8bit入っている
 		hash = {
-			:V =>pkt[0].unpack("B*")[0][0..1],
-			:P =>pkt[0].unpack("B*")[0][2],
-			:X =>pkt[0].unpack("B*")[0][3],
-			:CC =>pkt[0].unpack("B*")[0][4..7],
-			:M =>pkt[1].unpack("B*")[0][0],
-			:PT =>pkt[1].unpack("B*")[0][1..7],
-			:sequence_number =>pkt[2..3].unpack("B*")[0],
+			:V =>pkt[0].unpack("B*")[0][0..1], # Version
+			:P =>pkt[0].unpack("B*")[0][2], # Padding
+			:X =>pkt[0].unpack("B*")[0][3], # eXtension: 拡張ヘッダがある場合は1
+			:CC =>pkt[0].unpack("B*")[0][4..7], # Contributing source Count
+			:M =>pkt[1].unpack("B*")[0][0], # Marker: jpegペイロードの最後のみ1
+			:PT =>pkt[1].unpack("B*")[0][1..7], # PayloadType
+			:sequence_number =>pkt[2..3].unpack("B*")[0], 
 			:timestamp =>pkt[4..7].unpack("B*")[0],
-			:SSRC =>pkt[8..11].unpack("B*")[0]
+			:SSRC =>pkt[8..11].unpack("B*")[0] # 送信元識別子
 		}
-			#:enhance =>pkt[0].unpack("B*")[0][0..1]
-			#:JPEG_subframe =>0pkt[0].unpack("B*")[0][0..1]
-		# 先頭パケット 拡張ヘッダを持つ
+		# JPEGフレームの先頭パケット 拡張ヘッダを持つ
 		if hash[:X] == "1"
 			#拡張ヘッダの処理
-			#TODO とりあえず拡張ヘッダをスキップする
+			#TODO とりあえず拡張ヘッダは無視する
 			for i in 12..pkt.size
 				j = i+1
 				b = pkt[i..j].unpack("B*")[0] #16bitずつチェック
@@ -69,7 +63,6 @@ class RTPParser
 			@@jpeg = []
 			connect if @ws.nil?
 			@ws.send base
-			#File.write("test.txt", base)
 		# 途中パケット
 		else
 			# ヘッダを除いたパケット(==JPEGサブフレーム)を生バイナリで@@jpegに追加
--- opc_udp.rb
+++ opc_udp.rb
@@ -14,7 +14,6 @@ class UdpServer < EM::Connection
 	# override データを受信する度に実行される
 	def receive_data data
 		parseRTP data
-		#@ws.send data
 	end
 
 	def self.run
@@ -25,35 +24,6 @@ class UdpServer < EM::Connection
 	end
 
 	def parseRTP pkt
-		#p pkt[0].unpack("B*")[0][0..1] # VVPXCCCC
-		#p pkt[1].unpack("B*")[0] # MPTPTPTP
-		#p pkt[2..3].unpack("B*")[0] # sequence num
-		#p pkt[4..7].unpack("B*")[0] # timestanp 
-		#p pkt[8..11].unpack("B*")[0] # SSRC
-		#p pkt[12..13].unpack("B*")[0] # 拡張ヘッダのVersion
-		#p pkt[14..15].unpack("B*")[0] # 拡張ヘッダのlength
-		
 		@@parser.parse pkt
-
-		#for i in 0..pkt.size
-		#	j = i+1
-		#	b = pkt[i..j].unpack("B*")[0] # SSRC
-		#	#p b
-		#	if b == "1111111111011000" # FFD8
-		#		p "start"
-		#		@@jpeg = 1
-		#	end
-		#	if @@jpeg == 1
-		#	#	p b
-		#	end
-		#	if b == "1111111111011001" # FFD9
-		#		#p "end"
-		#		#exit 1
-		#	end
-		#end
-
-		#p pkt
-		#p pkt.unpack("N*").pack("V*").unpack("B*")
-		#exit 1
 	end
 end
